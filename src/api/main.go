2a60826584f685babc8747191ea33f00ffb152a7
Takaaki Matsushita <takaaki@Takaakis-iMac.local>
2021-11-25 09:52:47 +0900

    Update Dokcerfile
    I try to execute go and gin application without mysql .

--- src/api/main.go
+++ src/api/main.go
@@ -1,10 +1,15 @@
 package main
 
-import (
-	"fmt"
-)
+import "github.com/gin-gonic/gin"
+
+import "net/http"
 
 func main() {
-	fmt.Println("Hello World!")
-	fmt.Println("Good evening!")
+    engine:= gin.Default()
+    engine.GET("/", func(c *gin.Context) {
+        c.JSON(http.StatusOK, gin.H{
+            "message": "hello world",
+        })
+    })
+    engine.Run(":3000")
 }
